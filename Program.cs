eb2d92b4ab4b3bf60dc9967077cbd8f5a94b0313
Micaela <micaela95@live.se>
2026-06-17 13:14:30 +0200

    test

--- Program.cs
+++ Program.cs
@@ -21,19 +21,50 @@ app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
-app.Use(async (context, next) =>
-{
-	var allowedReferrer = new Uri("http://room1.runasp.net/");
+// app.Use(async (context, next) => Ändring från pull
+// {
+// 	var allowedReferrer = new Uri("http://room1.runasp.net/");
 
-	if (!context.Request.Headers.TryGetValue("Referer", out var refererValue) ||
-		!Uri.TryCreate(refererValue.ToString(), UriKind.Absolute, out var refererUri) ||
-		refererUri.Host != allowedReferrer.Host)
+// 	if (!context.Request.Headers.TryGetValue("Referer", out var refererValue) ||
+// 		!Uri.TryCreate(refererValue.ToString(), UriKind.Absolute, out var refererUri) ||
+// 		refererUri.Host != allowedReferrer.Host)
+// 	{
+// 		context.Response.Redirect("http://room1.runasp.net/");
+// 		return;
+// 	}
+// 	await next();
+// });
+
+// app.Use(async (context, next) => gammal kod
+// {
+// 	var allowedReferrer = new Uri("http://room1.runasp.net/");
+
+// 	if (!context.Request.Headers.TryGetValue("Referer", out var refererValue) ||
+// 		!Uri.TryCreate(refererValue.ToString(), UriKind.Absolute, out var refererUri) ||
+// 		refererUri.Host != allowedReferrer.Host)
+// 	{
+// 		context.Response.Redirect("http://room1.runasp.net/");
+// 		return;
+// 	}
+// 	await next();
+// });
+
+if (!app.Environment.IsDevelopment())
+{
+	app.Use(async (context, next) =>
 	{
-		context.Response.Redirect("http://room1.runasp.net/");
-		return;
-	}
-	await next();
-});
+		var allowedReferrer = new Uri("http://room1.runasp.net/");
+
+		if (!context.Request.Headers.TryGetValue("Referer", out var refererValue) ||
+			!Uri.TryCreate(refererValue.ToString(), UriKind.Absolute, out var refererUri) ||
+			refererUri.Host != allowedReferrer.Host)
+		{
+			context.Response.Redirect("http://room1.runasp.net/");
+			return;
+		}
+		await next();
+	});
+}
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
